59f6fbca22493dfe80d10bf92b206549fff4cd31
John Nguyen <Johnsum41.1@gmail.com>
2019-07-01 15:14:32 -0400

    Step 1

--- rpg-0.py
+++ rpg-0.py
@@ -6,6 +6,15 @@ In this simple RPG game, the hero fights the goblin. He has the options to:
 3. flee
 
 """
+class Hero:
+   
+    def hero(self, health, power):
+        self.health = health
+        self.power = power
+
+    def goblin(self, health, power):
+        self.health = health
+        self.power = power
 
 def main():
     hero_health = 10
